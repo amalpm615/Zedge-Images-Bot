7f2e25cc1c18b63604d4e77e90f53697113ca24b
anandvfcvijay <49161738+anandvfcvijay@users.noreply.github.com>
2019-06-01 19:49:29 +0530

    Update and rename config.sample.php to config.php

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+
+// obtain bot token from @BotFather
+$TG_BOT_TOKEN = "784474247:AAHOZLS3BMCse7mattKhqEGs-efivtpSpUE";
--- config.sample.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-// obtain bot token from @BotFather
-$TG_BOT_TOKEN = "";
